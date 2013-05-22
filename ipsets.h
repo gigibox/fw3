c307c10ba8edb637c58db1449434fdd6b751f7f0
Jo-Philipp Wich <jow@openwrt.org>
2013-05-22 15:56:59 +0200

    Add required ipset declarations for kernels < 3.7

--- ipsets.h
+++ ipsets.h
@@ -39,4 +39,31 @@ bool fw3_check_ipset(struct fw3_ipset *set);
 #define fw3_free_ipset(ipset) \
 	fw3_free_object(ipset, fw3_ipset_opts)
 
+
+#ifndef SO_IP_SET
+
+#define SO_IP_SET           83
+#define IPSET_MAXNAMELEN    32
+#define IPSET_INVALID_ID    65535
+
+union ip_set_name_index {
+    char name[IPSET_MAXNAMELEN];
+    uint16_t index;
+};
+
+#define IP_SET_OP_GET_BYNAME    0x00000006
+struct ip_set_req_get_set {
+    uint32_t op;
+    uint32_t version;
+    union ip_set_name_index set;
+};
+
+#define IP_SET_OP_VERSION       0x00000100
+struct ip_set_req_version {
+    uint32_t op;
+    uint32_t version;
+};
+
+#endif /* SO_IP_SET */
+
 #endif
